ba26b7be006fd5f853e9b4d655ee3615478330dd
Alex <alexisburakov@gmail.com>
2019-09-24 16:35:12 +0300

    Updates

---
+++ precache-manifest.1670a37cc6da515671e78463691fb12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2bf0e1301538829cbca",
+    "url": "/ratingApp/static/css/main.96580e7a.chunk.css"
+  },
+  {
+    "revision": "d2bf0e1301538829cbca",
+    "url": "/ratingApp/static/js/main.17d46030.chunk.js"
+  },
+  {
+    "revision": "7c08a14b1e3f3afb1ece",
+    "url": "/ratingApp/static/js/runtime~main.e506d6b7.js"
+  },
+  {
+    "revision": "03a0f2c7d3d883001195",
+    "url": "/ratingApp/static/css/2.fca4e826.chunk.css"
+  },
+  {
+    "revision": "03a0f2c7d3d883001195",
+    "url": "/ratingApp/static/js/2.d89668b3.chunk.js"
+  },
+  {
+    "revision": "dfbe1044361753e4ac1a4f047b2e4c0f",
+    "url": "/ratingApp/index.html"
+  }
+];
